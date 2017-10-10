97371a36405b10a5c58ee7ba806f8a3442bd7071
adam cox <mr.adamcox@gmail.com>
2017-10-10 13:26:35 -0500

    remove and support settings.json outside of version control

--- scripts/ManageSettings.py
+++ scripts/ManageSettings.py
@@ -1,7 +1,11 @@
 import os
 import json
 import arcpy
-from clitools.config import settings
+
+try:
+    from clitools.config import settings
+except:
+    settings = {}
 
 settings['toolbox-location'] = arcpy.GetParameterAsText(0)
 settings['cli-gis-directory'] = arcpy.GetParameterAsText(1)
@@ -12,8 +16,12 @@ settings['default-constraint'] = arcpy.GetParameterAsText(5)
 settings['default-program-collection'] = arcpy.GetParameterAsText(6)
 
 if not os.path.isdir(settings['cli-gis-directory']):
-    os.makedirs(settings['cli-gis-directory'])
+    try:
+        os.makedirs(settings['cli-gis-directory'])
+    except:
+        raise Exception("Invalid CLI GIS Directory path, operation cancelled.")
 
 settings_file = os.path.join(settings['toolbox-location'],"scripts","clitools","config","settings.json")
+
 with open(settings_file, "wb") as out:
     json.dump(settings,out,indent=2)
