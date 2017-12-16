d389d919f296b78a0dff08eee950221d5e09caff
adam cox <mr.adamcox@gmail.com>
2017-12-15 19:34:47 -0600

    standardize calls to settings, add error handling re: #13

--- scripts/clitools/config/__init__.py
+++ scripts/clitools/config/__init__.py
@@ -3,6 +3,8 @@ import json
 
 def get_settings():
     settings_file = os.path.join(os.path.dirname(__file__),"settings.json")
+    if not os.path.isfile(settings_file):
+        return False
     with open(settings_file,'rb') as f:
         settings = json.load(f)
     return settings
