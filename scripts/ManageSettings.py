8f6d288e71a68d464f76ff5867b9365580d2dc9f
adam cox <mr.adamcox@gmail.com>
2017-12-29 08:02:27 -0600

    add log-dir setting to hold all logs

--- scripts/ManageSettings.py
+++ scripts/ManageSettings.py
@@ -9,26 +9,28 @@ if not settings:
 settings['toolbox-location'] = arcpy.GetParameterAsText(0)
 settings['cli-gis-directory'] = arcpy.GetParameterAsText(1)
 settings['cache'] = arcpy.GetParameterAsText(2)
-settings['default-edit-by'] = arcpy.GetParameterAsText(3)
-settings['default-map-method'] = arcpy.GetParameterAsText(4)
-settings['default-originator'] = arcpy.GetParameterAsText(5)
-settings['default-constraint'] = arcpy.GetParameterAsText(6)
-settings['default-program-collection'] = arcpy.GetParameterAsText(7)
-settings['trans-nad83-wsg84'] =  arcpy.GetParameterAsText(8)
-settings['trans-nad27-wsg84'] =  arcpy.GetParameterAsText(9)
+settings['log-dir'] = arcpy.GetParameterAsText(3)
+settings['default-edit-by'] = arcpy.GetParameterAsText(4)
+settings['default-map-method'] = arcpy.GetParameterAsText(5)
+settings['default-originator'] = arcpy.GetParameterAsText(6)
+settings['default-constraint'] = arcpy.GetParameterAsText(7)
+settings['default-program-collection'] = arcpy.GetParameterAsText(8)
+settings['trans-nad83-wsg84'] =  arcpy.GetParameterAsText(9)
+settings['trans-nad27-wsg84'] =  arcpy.GetParameterAsText(10)
 
-if not os.path.isdir(settings['cli-gis-directory']):
-    try:
-        os.makedirs(settings['cli-gis-directory'])
-    except:
-        raise Exception("Invalid CLI GIS Directory path, operation cancelled.")
+dirs = (
+    settings['cli-gis-directory'],
+    settings['cache'],
+    settings['log-dir']
+)
+
+for d in dirs:
+    if not os.path.isdir(d):
+        try:
+            os.makedirs(d)
+        except WindowsError:
+            raise Exception("Invalid CLI GIS Directory path, operation cancelled.")
         
-if not os.path.isdir(settings['cache']):
-    try:
-        os.makedirs(settings['cache'])
-    except:
-        pass
-
 settings_file = os.path.join(settings['toolbox-location'],"scripts","clitools","config","settings.json")
 
 with open(settings_file, "wb") as out:
