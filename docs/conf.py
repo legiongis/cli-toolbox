d84c6057c567e5d2fd135aab8a329251b8f767f3
adam cox <mr.adamcox@gmail.com>
2018-08-31 17:01:26 -0500

    add custom docs style, update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CLI Toolbox'
-copyright = u'2017, Legion GIS, LLC'
+copyright = u'2018, Legion GIS, LLC'
 author = u'Legion GIS, LLC'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -105,6 +105,8 @@ if not on_rtd:
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_style = 'css/style.css'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
